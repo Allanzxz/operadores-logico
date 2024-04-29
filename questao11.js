8e7cd3596af421b874d89bcc494a6257c50acb11
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-29 11:28:45 -0400

    finalizada questao 11

---
+++ questao11.js
@@ -0,0 +1,5 @@
+/* 
+11. Explique a diferença entre os operadores && e || em JavaScript. 
+*/
+//resposta: true && true retorna true, porque ambos os operandos são verdadeiros.
+//true || false retorna true, porque pelo menos um dos operandos é verdadeiro.
