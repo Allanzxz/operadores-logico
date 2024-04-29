c17a7af15cb7abc9675a2cb31989c36cdacb4695
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-29 11:30:00 -0400

    finalizada questao 26

---
+++ questao26.js
@@ -0,0 +1,5 @@
+/*
+26. Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript. 
+*/
+// resposta os parênteses são uma ferramenta poderosa em JavaScript para controlar a ordem de avaliação e a lógica das expressões com operadores lógicos, ajudando a garantir que o código seja executado conforme esperado e que as expressões sejam claras e compreensíveis
