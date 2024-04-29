8c1e8ec74e2ba9483bd6d055091deec15d0506ec
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-29 11:29:05 -0400

    finalizada questao 15

---
+++ questao15.js
@@ -0,0 +1,5 @@
+/* 
+15. Explique como a ordem de avaliação dos operadores lógicos afeta o 
+resultado de uma expressão em JavaScript. 
+*/
+//resposta: A ordem de avaliação dos operadores lógicos em JavaScript pode afetar o resultado de uma expressão, especialmente quando há combinação de operadores diferentes, como && (E) e || (OU).
